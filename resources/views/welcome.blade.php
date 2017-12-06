47ee27b3e07449094a07dd535f897bb50ccde4d5
vagrant <vagrant@homestead>
2017-12-06 12:25:13 +0000

    Create auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LaravelDev
                 </div>
 
                 <div class="links">
